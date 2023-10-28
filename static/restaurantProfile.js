caf16669af4ef569843ad1b99037bb132b979a06
aayushdhakal69 <therespawner69@gmail.com>
2023-10-28 13:55:13 +0545

    json added

---
+++ static/restaurantProfile.js
@@ -0,0 +1,15 @@
+// fetch("bhimbhaiitems.json")
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (bhimbhaiitems) {
+//     let placeholder = document.querySelector("#data-output");
+//     let out = "";
+//     for (let item of bhimbhaiitems) {
+//         out += `
+//         ${item.price}
+//     `;
+//     }
+
+//     placeholder.innerHTML = out;
+// });
